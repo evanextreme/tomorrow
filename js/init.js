a5e1169fbedae2adb6c3c8416ccfa725794a09a9
Evan Hirsh <contact@ehirsh.com>
2015-01-23 14:29:05 -0500

    All toasters toast, toast!
    Toast is such a weird word.

--- js/init.js
+++ js/init.js
@@ -3,6 +3,11 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+ 
+ $(document).ready(function(){
+    $('.tooltipped').tooltip({"delay": 150});
+  });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
