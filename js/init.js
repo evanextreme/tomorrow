4826df51f399e4340a0116523b7485fec48e3337
Corbin Davenport <davenportcorbin@gmail.com>
2015-01-27 22:13:31 -0500

    fully working welcome page

--- js/init.js
+++ js/init.js
@@ -9,18 +9,32 @@ $(document).ready(function(){
 	// Make sure localStorage variablies exist
 
 	if (localStorage.getItem("firstname") === null) {
-		localStorage['firstname'] = 'Unknown';	
+		localStorage['firstname'] = '';	
 	}
-	if (localStorage.getItem("lasttname") === null) {
-		localStorage['lastename'] = 'Unknown';	
+	if (localStorage.getItem("lastname") === null) {
+		localStorage['lastname'] = '';	
 	}
 	if (localStorage.getItem("zip") === null) {
-		localStorage['zip'] = 'Unknown';	
+		localStorage['zip'] = '';	
 	}
 
 	// Setup code
 
 	if ( $( "body" ).is( ".setup" ) ) {
-		// do stuff
+		document.getElementById("first_name").value = localStorage['firstname'];
+		document.getElementById("last_name").value = localStorage['lastname'];
+		document.getElementById("zip").value = localStorage['zip'];
 	}
+
+	$('#getstarted').click(function() {
+		if( (document.getElementById("first_name").value == 0) || (document.getElementById("last_name").value == 0) || (document.getElementById("zip").value == 0) )
+		{
+		   toast('Fill out everything and try again!', 10000000, 'rounded');
+		} else {
+			localStorage['firstname'] = document.getElementById("first_name").value;
+			localStorage['lastname'] = document.getElementById("last_name").value;
+			localStorage['zip'] = document.getElementById("zip").value;
+			window.location.replace("today.html");
+		}
+	});
 });
