5f971254aefa7216ddf7642c0a3065bc0498b510
Corbin Davenport <davenportcorbin@gmail.com>
2015-01-27 21:36:14 -0500

    fixed broken links/bad syntax

--- js/init.js
+++ js/init.js
@@ -1,13 +1,5 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
- 
- $(document).ready(function(){
-    $('.tooltipped').tooltip({"delay": 150});
-  });
-
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+	$('.button-collapse').sideNav();
+	$('.parallax').parallax();
+	$('.tooltipped').tooltip({"delay": 150});
+});
