95fb41fcad000a627fb4792b87615e7ba360737c
Corbin Davenport <davenportcorbin@gmail.com>
2015-01-28 15:12:35 -0500

    added comments

--- js/init.js
+++ js/init.js
@@ -18,19 +18,23 @@ $(document).ready(function(){
 		localStorage['zip'] = '';	
 	}
 
-	// Setup code
+	// Code for setup process
 
 	if ( $( "body" ).is( ".setup" ) ) {
+		// Sets contents of text fields on setup screen to localStorage
 		document.getElementById("first_name").value = localStorage['firstname'];
 		document.getElementById("last_name").value = localStorage['lastname'];
 		document.getElementById("zip").value = localStorage['zip'];
 	}
-
+	
 	$('#getstarted').click(function() {
+		// Test if any of the text fields are empty
 		if( (document.getElementById("first_name").value == 0) || (document.getElementById("last_name").value == 0) || (document.getElementById("zip").value == 0) )
 		{
-		   toast('Fill out everything and try again!', 5000, 'rounded');
+			// Tell user to fill out everything
+			toast('Fill out everything and try again!', 5000, 'rounded');
 		} else {
+			// Save contents of text fields to localStorage
 			localStorage['firstname'] = document.getElementById("first_name").value;
 			localStorage['lastname'] = document.getElementById("last_name").value;
 			localStorage['zip'] = document.getElementById("zip").value;
