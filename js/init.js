0f80308ee385af7f8920d92c51648ee24e4ceee5
Corbin Davenport <davenportcorbin@gmail.com>
2015-01-27 21:42:56 -0500

    Moved logowhite.png, begin setup JS

--- js/init.js
+++ js/init.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
+
+	// Initialize buttons
+
 	$('.button-collapse').sideNav();
 	$('.parallax').parallax();
 	$('.tooltipped').tooltip({"delay": 150});
+
+	// Make sure localStorage variablies exist
+
+	if (localStorage.getItem("firstname") === null) {
+		localStorage['firstname'] = 'Unknown';	
+	}
+	if (localStorage.getItem("lasttname") === null) {
+		localStorage['lastename'] = 'Unknown';	
+	}
+	if (localStorage.getItem("zip") === null) {
+		localStorage['zip'] = 'Unknown';	
+	}
+
+	// Setup code
+
+	if ( $( "body" ).is( ".setup" ) ) {
+		// do stuff
+	}
 });
