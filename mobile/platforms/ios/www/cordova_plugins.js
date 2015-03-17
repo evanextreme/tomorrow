6f88e708441c894eadadded13326ac9ea5774608
evanextreme <evanextreme@aim.com>
2015-03-16 21:59:07 -0400

    Rebuilt apps on newest Cordova instead of phonegap

--- mobile/platforms/ios/www/cordova_plugins.js
+++ mobile/platforms/ios/www/cordova_plugins.js
@@ -1,25 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
-        "id": "org.apache.cordova.statusbar.statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "org.apache.cordova.statusbar": "0.1.10",
-    "org.apache.cordova.inappbrowser": "0.6.0"
-}
+{}
 // BOTTOM OF METADATA
 });
